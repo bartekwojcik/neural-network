f2c08c4956ce49e2ec8948633687e5913375c579
bartek <kicjow.ketrab@gmail.com>
2018-05-12 20:55:41 +0100

    predicting if this one guy will leave the bank

--- ann.py
+++ ann.py
@@ -1,18 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as pl
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import keras
 from keras.models import Sequential
 from keras.layers import Dense
 from keras import metrics
+from sklearn.metrics import confusion_matrix
+from keras.models import model_from_json
 import sys
 
 
-def main(args):
+def train(should_train):
     dataset = pd.read_csv('Churn_Modelling.csv')
     x = dataset.iloc[:, 3:13].values
     y = dataset.iloc[:, 13].values
@@ -35,25 +34,54 @@ def main(args):
     x_test = sc.transform(x_test)
 
     # train network
+    if should_train == "True": #lol python
+        classifier = Sequential()
+        # adding hidden layer
+        classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_dim=11))
+        # adding SECOND HIDDEN layer
+        classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+        # OUPUT layer
+        classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+        # compile
+        classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=[metrics.binary_accuracy])
+        # train
+        classifier.fit(x_train, y_train, batch_size=10, epochs=15)  # slow virtual machine
+        # tell me the truth
+        y_pred = classifier.predict(x_test)
+        y_pred = (y_pred > 0.5)
 
-    classifier = Sequential()
-    # adding hidden layer
-    classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_dim=11))
-    # adding SECOND HIDDEN layer
-    classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
-    # OUPUT layer
-    classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
-    # compile
-    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=[metrics.binary_accuracy])
-    # train
-    classifier.fit(x_train, y_train, batch_size=10, epochs=30) #slow virtual machine
-    # tell me the truth
-    y_pred = classifier.predict(x_test)
-    y_pred = (y_pred > 0.5)
+        cm = confusion_matrix(y_test, y_pred)
+        print(*cm)
 
-    from sklearn.metrics import confusion_matrix
-    cm = confusion_matrix(y_test, y_pred)
+        model_json = classifier.to_json()
+        with open("/home/bartek/PycharmProjects/ann/model.json","w") as json_file:
+            json_file.write(model_json)
+        classifier.save_weights("/home/bartek/PycharmProjects/ann/model.h5")
+        print("Saved model to disk")
+
+    json_file = open('/home/bartek/PycharmProjects/ann/model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("/home/bartek/PycharmProjects/ann/model.h5")
+    print("Loaded model from disk")
+
+    loaded_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[metrics.binary_accuracy])
+    thisOneGuy = np.array([[600, 'France', 'Male', 40, 3, 60000, 2, 1, 1, 50000]])
+    thisOneGuy[:,1] =  labelencoder_X_1.transform(thisOneGuy[:,1])
+    thisOneGuy[:,2] = labelencoder_X_2.transform(thisOneGuy[:,2])
+    thisOneGuy = onehotencoder.transform(thisOneGuy).toarray()
+    thisOneGuy = thisOneGuy[:,1:]
+
+    thisOneGuy = sc.transform(thisOneGuy)
+
+    guyPred = loaded_model.predict(thisOneGuy)
     debug =5
+    print(guyPred)
+
 
 if __name__ == '__main__':
-    main(sys.argv)
+    should_train = sys.argv[1]
+    train(should_train)
+
